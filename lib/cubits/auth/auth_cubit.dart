b4317aaccc61d144771dccb433d7c599039da475
Bikram <dasbikram303@gmail.com>
2026-02-10 16:35:10 +0545

    Update on 10 Feb 2026

--- lib/cubits/auth/auth_cubit.dart
+++ lib/cubits/auth/auth_cubit.dart
@@ -1,8 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
-  AuthCubit() : super(AuthInitial());
+  AuthCubit() : super(AuthState.initial());
 }
--- lib/cubits/auth/auth_state.dart
+++ lib/cubits/auth/auth_state.dart
@@ -1,10 +1,39 @@
 part of 'auth_cubit.dart';
 
-sealed class AuthState extends Equatable {
-  const AuthState();
+enum AuthStatus {
+  unknown,
+  authenticated,
+  unauthenticated,
 }
 
-final class AuthInitial extends AuthState {
+class AuthState extends Equatable {
+  final User? user;
+  final AuthStatus authStatus;
+
+  const AuthState({
+    this.user,
+    required this.authStatus,
+  });
+
+  factory AuthState.initial() {
+    return const AuthState(authStatus: AuthStatus.unknown);
+  }
+
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [user, authStatus];
+
+  @override
+  String toString() {
+    return 'AuthState{user: $user, authStatus: $authStatus}';
+  }
+
+  AuthState copyWith({
+    User? user,
+    AuthStatus? authStatus,
+  }) {
+    return AuthState(
+      user: user ?? this.user,
+      authStatus: authStatus ?? this.authStatus,
+    );
+  }
 }
